5ba903c3ffa3177ada5004842f67db450354c4e5
Dieme-Musabyimana <musabyimanadieme10@gmail.com>
2026-06-02 19:09:49 +0200

    Extract time stamp, level and message from the app.log

--- log_analyzer.py
+++ log_analyzer.py
@@ -5,5 +5,15 @@ file = open(file_name)
 
 for line in file:
     print(line.strip())
+    part = line.strip().split()
+    time_stamp = part[0] + " " + part[1]
+    level = part[2]
+    message = part[3]
+    print("time stamp: " + time_stamp)
+    print("level: " + level)
+    print("message: " + message)
+
+
+
 
 file.close()
